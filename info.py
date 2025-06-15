ef3ae6eeeed498ad2da0b730428bc3ba9ef16000
farookhannah <farookhannah@gmail.com>
2025-06-15 18:04:53 +0300

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+BN500QqPgDRlZWNk')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+yCBlvcWwObI0NDk0')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '@Farookhannah') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', '@Farookhannah)
+OWNER_LNK = environ.get('OWNER_LNK', '@Farookhannah')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
